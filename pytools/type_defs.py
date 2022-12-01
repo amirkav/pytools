f66dab5ca425ebb02017d5d64f1e2fc37865293a
Amir Kavousian <kav.amir@gmail.com>
2022-12-01 11:58:15 -0800

    Add type definitions

---
+++ pytools/type_defs.py
@@ -0,0 +1,19 @@
+from typing import (  # pylint: disable=no-name-in-module
+    Literal,
+    NoReturn,
+    Protocol,
+    TypedDict,
+    get_args,
+    get_origin,
+    runtime_checkable,
+)
+
+__all__ = (
+    "Literal",
+    "NoReturn",
+    "Protocol",
+    "TypedDict",
+    "get_args",
+    "get_origin",
+    "runtime_checkable",
+)
